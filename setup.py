d0c14c85615b4c30d120e60e966993eab99ab80d
Nathan Oliver <nathan.oliver@iotecglobal.com>
2018-09-22 13:35:01 +0100

    fixed bad setup.py

--- setup.py
+++ setup.py
@@ -12,15 +12,32 @@ with open(os.path.join(about['here'], 'version.py')) as f:
 with open(os.path.join(about['here'], 'test', '__init__.py')) as f:
     exec (f.read(), about)
 
+with open(os.path.join(about['here'], 'README.md')) as f:
+    about['readme'] = f.read()
+
 
 setup(
+    # available in PKG-INFO
     name='xcm',
     version=about['__version__'],
     description='Cross Customer Model machine learning for Realtime Bidding',
+    url='https://github.com/iotgdev/xcm/',
     author='iotec',
     author_email='dev@dsp.io',
-    url='https://github.com/iotgdev/xcm/',
+    license='MIT',
     download_url='https://github.com/iotgdev/xcm/archive/{}.tar.gz'.format(about['__version__']),
+    long_description=about['readme'],
+    platforms=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+
+    # Package Properties
     packages=find_packages(include=['xcm', 'xcm.*']),
     include_package_data=True,
     test_suite='test',
